6b53b30c3e402b675edaf1f7dce2c5f98fa5c312
Virxt-18 <demonvirat1980@gmail.com>
2026-09-05 21:17:45 +0530

    render fix

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import { buildApp } from './app';
-import { env } from './config/env';
+import { buildApp } from "./app";
+import { env } from "./config/env";
 
 const app = buildApp();
 
-app.listen({ host: env.HOST, port: env.PORT }).then(() => {
-  app.log.info(`AirPulse API listening on ${env.HOST}:${env.PORT}`);
-}).catch((error) => {
-  app.log.error(error);
-  process.exit(1);
-});
+app
+  .listen({ host: "0.0.0.0", port: env.PORT || 3000 })
+  .then(() => {
+    app.log.info(`AirPulse API listening on ${env.HOST}:${env.PORT}`);
+  })
+  .catch((error) => {
+    app.log.error(error);
+    process.exit(1);
+  });
